251fed94c86de57264db047f13949d3ec96f6308
EGOIST <0x142857@gmail.com>
2016-05-22 18:58:09 +0800

    Updates

---
+++ index.js
@@ -0,0 +1,66 @@
+(function() {(window.nunjucksPrecompiled = window.nunjucksPrecompiled || {})["index.html"] = (function() {
+function root(env, context, frame, runtime, cb) {
+var lineno = null;
+var colno = null;
+var output = "";
+try {
+var parentTemplate = null;
+env.getTemplate("base.html", true, "index.html", false, function(t_2,_parentTemplate) {
+if(t_2) { cb(t_2); return; }
+parentTemplate = _parentTemplate
+for(var t_1 in parentTemplate.blocks) {
+context.addBlock(t_1, parentTemplate.blocks[t_1]);
+}
+output += "\n\n";
+(parentTemplate ? function(e, c, f, r, cb) { cb(""); } : context.getBlock("nav"))(env, context, frame, runtime, function(t_4,t_3) {
+if(t_4) { cb(t_4); return; }
+output += t_3;
+output += "\n\n";
+(parentTemplate ? function(e, c, f, r, cb) { cb(""); } : context.getBlock("main"))(env, context, frame, runtime, function(t_6,t_5) {
+if(t_6) { cb(t_6); return; }
+output += t_5;
+output += "\n";
+if(parentTemplate) {
+parentTemplate.rootRenderFunc(env, context, frame, runtime, cb);
+} else {
+cb(null, output);
+}
+})})});
+} catch (e) {
+  cb(runtime.handleError(e, lineno, colno));
+}
+}
+function b_nav(env, context, frame, runtime, cb) {
+var lineno = null;
+var colno = null;
+var output = "";
+try {
+output += "\n  ";
+output += runtime.suppressValue((lineno = 3, colno = 6, runtime.callWrap(runtime.contextOrFrameLookup(context, frame, "nav"), "nav", context, ["Home"])), env.opts.autoescape);
+output += "\n";
+cb(null, output);
+;
+} catch (e) {
+  cb(runtime.handleError(e, lineno, colno));
+}
+}
+function b_main(env, context, frame, runtime, cb) {
+var lineno = null;
+var colno = null;
+var output = "";
+try {
+output += "\n  <p>\n    While BootStrap is about 86KB large, the size of BootCSS will be smaller than half of it, 40KB hopefully.\n  </p>\n\n  <p>\n    Brought to you by <a href=\"https://github.com/egoist\">EGOIST</a>, a lovely guy people love.\n  </p>\n";
+cb(null, output);
+;
+} catch (e) {
+  cb(runtime.handleError(e, lineno, colno));
+}
+}
+return {
+b_nav: b_nav,
+b_main: b_main,
+root: root
+};
+
+})();
+})();
